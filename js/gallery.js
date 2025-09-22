291e264ff73b734788debb1c088e2360afc201f3
Olgerd82 <49753851+Olgerd82@users.noreply.github.com>
2025-09-22 19:15:26 +0200

    swipe gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,14 @@
-// GALLERY 
-
-const images = [
-  'img/bathroom1.jpg',
-  'img/bathroom2.jpg',
-  'img/bathroom3.jpg',
-  'img/bathroom4.jpg',
-  'img/bathroom5.jpg'
-];
-
-let current = 0;
-const sliderImage = document.getElementById('slider-image');
-
-function showImage(index) {
-  sliderImage.src = images[index];
-}
-
-function nextImage() {
-  current = (current + 1) % images.length;
-  showImage(current);
-}
-
-function prevImage() {
-  current = (current - 1 + images.length) % images.length;
-  showImage(current);
-}
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
